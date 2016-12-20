497263002cb8fd736cecb930fdbce63230b581c5
Daniel Hu <hdc@pantum.com>
2016-12-20 10:49:31 +0800

    cmake import library ok

--- app/imported-lib/import-lib.h
+++ app/imported-lib/import-lib.h
@@ -1,6 +1,14 @@
 #ifndef IMPORT_LIB_H
 #define IMPORT_LIB_H
 
+#  ifdef __cplusplus
+extern "C" {
+#  endif /* __cplusplus */
+
 int mysub(int a, int b);
+
+#  ifdef __cplusplus
+}
+#  endif /* __cplusplus */
 
 #endif
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,11 +10,11 @@ jstring
 Java_com_example_hdc_myapplication_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++ ";
     int a = myAdd(1, 2) + mysub(5, 2);
-    AndroidBitmapInfo bmpInfo = {0};
-    jobject  bmpObj;
-    AndroidBitmap_getInfo(env, bmpObj, &bmpInfo);
+    std::string hello = "Hello from C++ ";
+    //AndroidBitmapInfo bmpInfo = {0};
+    //jobject  bmpObj;
+    //AndroidBitmap_getInfo(env, bmpObj, &bmpInfo);
     return env->NewStringUTF(hello.c_str());
 }
 
